08afc2e54e736ecf5f0b749664fb701ab0c17b18
minho jeong <wjdalsgh6951@gmail.com>
2023-10-13 15:16:43 +0900

    project init

---
+++ src/main/java/com/grungapda/GrungapdaApplication.java
@@ -0,0 +1,13 @@
+package com.grungapda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrungapdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrungapdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grungapda/GrungapdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grungapda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrungapdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
