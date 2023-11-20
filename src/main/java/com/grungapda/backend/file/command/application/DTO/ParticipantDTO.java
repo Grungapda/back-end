98fb6db84c6d42e99a49b5e117e6e02ed776f599
anichan <qufekf97@gmail.com>
2023-11-20 10:58:00 +0900

    참가자 엔티티, DTO 수정 및 작성

---
+++ src/main/java/com/grungapda/backend/file/command/application/DTO/ParticipantDTO.java
@@ -0,0 +1,19 @@
+package com.grungapda.backend.file.command.application.DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParticipantDTO {
+
+    private Long userNo;
+
+    private Integer sessionType;
+
+}
